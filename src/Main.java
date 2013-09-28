23e098b1f2780df8c144c918c0b671af893be566
Chris Wendt <chrismwendt@gmail.com>
2013-09-28 14:28:03 -0400

    Created a JFrame with an ImagePanel inside

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+import java.awt.Dimension;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+
+public class Main extends JFrame {
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		ImagePanel imagePanel = new ImagePanel();
+		
+		getContentPane().add(imagePanel);
+		pack();
+		
+		setVisible(true);
+	}
 }
