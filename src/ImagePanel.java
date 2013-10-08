923c69c795b59608f84d32f303c13b1a983ec882
Chris Wendt <chrismwendt@gmail.com>
2013-10-08 17:54:34 -0400

    Applied formatting and code style

--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -1,44 +1,39 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class ImagePanel extends JPanel {
-	BufferedImage image;
-	BackgroundRemover bR;
-	public ImagePanel() {
-		setPreferredSize(new Dimension(800, 600));
-	}
+    BufferedImage image;
+    BackgroundRemover bR;
 
-	public void paint(Graphics g) {
-		g.setColor(Color.BLACK);
-		if (image != null) {
-			g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
-		}
-	}
+    public ImagePanel() {
+        setPreferredSize(new Dimension(800, 600));
+    }
 
-	public void loadImage(File selectedFile) {
-		if (selectedFile == null) {
-			return;
-		}
-		try {
-			image = ImageIO.read(selectedFile);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		bR=new BackgroundRemover(image);
-		bR.calculate();
-		repaint();
-	}
+    public void paint(Graphics g) {
+        g.setColor(Color.BLACK);
+        if (image != null) {
+            g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+        }
+    }
 
-
-
+    public void loadImage(File selectedFile) {
+        if (selectedFile == null) {
+            return;
+        }
+        try {
+            image = ImageIO.read(selectedFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        bR = new BackgroundRemover(image);
+        bR.calculate();
+        repaint();
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,40 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
-import com.sun.org.apache.bcel.internal.generic.LoadInstruction;
-
-
 public class Main extends JFrame implements ActionListener {
-	JFileChooser fileChooser;
-	JButton loadImageButton;
-	ImagePanel imagePanel;
-	
-	public static void main(String[] args) {
-		new Main();
-	}
-	
-	public Main() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new BorderLayout());
-		
-		loadImageButton = new JButton("Load image...");
-		imagePanel = new ImagePanel();
-		
-		getContentPane().add(loadImageButton, BorderLayout.NORTH);
-		getContentPane().add(imagePanel, BorderLayout.CENTER);
-		pack();
-		
-		loadImageButton.addActionListener(this);
-		fileChooser = new JFileChooser(System.getProperty("user.dir"));
-		
-		setVisible(true);
-	}
+    JFileChooser fileChooser;
+    JButton loadImageButton;
+    ImagePanel imagePanel;
 
-	public void actionPerformed(ActionEvent arg0) {
-		if (arg0.getSource() == loadImageButton) {
-			fileChooser.showOpenDialog(Main.this);
-			imagePanel.loadImage(fileChooser.getSelectedFile());
-		}
-	}
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        loadImageButton = new JButton("Load image...");
+        imagePanel = new ImagePanel();
+
+        getContentPane().add(loadImageButton, BorderLayout.NORTH);
+        getContentPane().add(imagePanel, BorderLayout.CENTER);
+        pack();
+
+        loadImageButton.addActionListener(this);
+        fileChooser = new JFileChooser(System.getProperty("user.dir"));
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent arg0) {
+        if (arg0.getSource() == loadImageButton) {
+            fileChooser.showOpenDialog(Main.this);
+            imagePanel.loadImage(fileChooser.getSelectedFile());
+        }
+    }
 }
