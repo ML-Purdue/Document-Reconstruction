8d0bfe5b987201554ac44846d4e00dfa65ba051b
Nicki Ogg <oggnik@gmail.com>
2013-10-12 15:25:03 -0400

    Fixed blob detection, added coloring

--- src/BlobDetector.java
+++ src/BlobDetector.java
@@ -1,12 +1,14 @@
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Stack;
 import javax.imageio.ImageIO;
 
 public class BlobDetector {
     private BufferedImage baseImage;
     private int[][] blobs;
-    public static final int BLOB_THRESHOLD = 50;
+    public static final int BLOB_THRESHOLD = 0;
 
     public BlobDetector(BufferedImage bi) {
         baseImage = bi;
@@ -17,36 +19,63 @@ public class BlobDetector {
         int blobNum = 0;
         // The = 1, < size -1
         // Are for not having to do inbounds checks for each neighbor
-        for (int x = 1; x < baseImage.getWidth() - 1; x++) {
-            for (int y = 1; y < baseImage.getHeight() - 1; y++) {
+        Stack<Pixel> pixelStack = new Stack<Pixel>();
+        for (int x = 0; x < baseImage.getWidth() - 1; x++) {
+            for (int y = 0; y < baseImage.getHeight() - 1; y++) {
                 if (blobs[x][y] != 0) {
                     continue;
                 }
                 if (getAlphaValue(baseImage.getRGB(x, y)) <= BLOB_THRESHOLD) {
                     continue;
                 }
-                // Check neighbors
-                // If neighbor is not transparent, set to highest neighbor number
-                // If neighbors == transparent, set own blob color
-                int highestNeighbor = 0;
-                for (int i = x - 1; i < x + 1; i++) {
-                    for (int j = y - 1; j < y + 1; j++) {
-                        if (blobs[i][j] > highestNeighbor) {
-                            highestNeighbor = blobs[i][j];
+                /*
+                 * For every pixel not part of a blob Add the pixel to a stack While the stack isn't empty, pop off the pixel, mark it as a blob, add its non-transparent neighbors
+                 */
+                pixelStack.push(new Pixel(x, y));
+
+                blobNum++;
+                while (!pixelStack.empty()) {
+                    Pixel currentPixel = pixelStack.pop();
+                    // System.out.printf("Popped: %d %d\n", currentPixel.x, currentPixel.y);
+                    blobs[currentPixel.x][currentPixel.y] = blobNum;
+                    // System.out.println(getAlphaValue(baseImage.getRGB(currentPixel.x, currentPixel.y)));
+                    for (int i = currentPixel.x - 1; i <= currentPixel.x + 1; i++) {
+                        for (int j = currentPixel.y - 1; j <= currentPixel.y + 1; j++) {
+                            // System.out.printf("dsa: %d %d\n", i, j);
+                            if (i < 0 || j < 0 || i >= baseImage.getWidth() || j >= baseImage.getHeight()) {
+                                continue;
+                            }
+                            // System.out.println("dsaf");
+                            if (blobs[i][j] != 0) {
+                                // System.out.println((i == currentPixel.x) + " " + (j == currentPixel.y) + " " + getAlphaValue(baseImage.getRGB(i, j)));
+                                continue;
+                            }
+
+                            if (getAlphaValue(baseImage.getRGB(i, j)) <= BLOB_THRESHOLD) {
+                                continue;
+                            }
+
+                            // System.out.printf("Pushing: %d %d\n", i, j);
+                            pixelStack.push(new Pixel(i, j));
                         }
                     }
+                    // System.exit(1);
                 }
-                if (highestNeighbor > 0) {
-                    blobs[x][y] = highestNeighbor;
-                } else {
-                    blobNum++;
-                    blobs[x][y] = blobNum;
-                }
+
+                /*
+                 * Iterative Method, creates too many blobs // Check neighbors // If neighbor is not transparent, set to highest neighbor number // If neighbors == transparent, set own blob color int highestNeighbor = 0; for (int i = x - 1; i < x + 1; i++) { for (int j = y - 1; j < y + 1; j++) { if (blobs[i][j] > highestNeighbor) { highestNeighbor = blobs[i][j]; } } } if (highestNeighbor > 0) { blobs[x][y] = highestNeighbor; } else { blobNum++; blobs[x][y] = blobNum; }
+                 */
 
                 /*
                  * //Recursive (stack overflow) if (blobs[x][y] != 0) continue; if (getAlphaValue(baseImage.getRGB(x, y)) <= BLOB_THRESHOLD) continue; System.out.println(x + " " + y); fillBlob(x, y, blobNum); blobNum++;
                  */
             }
+        }
+        System.out.println((blobNum) + " blobs found");
+        int[] blobColors = new int[blobNum + 1];
+        for (int i = 0; i < blobColors.length; i++) {
+            Color randomColor = new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
+            blobColors[i] = randomColor.getRGB();
         }
         System.out.println("Creating outImage");
         BufferedImage outImage = new BufferedImage(baseImage.getWidth(), baseImage.getHeight(), BufferedImage.TYPE_INT_RGB);
@@ -55,8 +84,11 @@ public class BlobDetector {
                 if (blobs[x][y] == 0) {
                     outImage.setRGB(x, y, 0xFFFFFFFF);
                 } else {
-                    outImage.setRGB(x, y, 0);
+                    outImage.setRGB(x, y, blobColors[blobs[x][y]]);
                 }
+                /*
+                 * if (blobs[x][y] == 1) { System.out.printf("x: %d, y: %d\n", x, y); }
+                 */
             }
         }
         System.out.println("Saving");
@@ -66,6 +98,7 @@ public class BlobDetector {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        System.out.println("done saving");
     }
 
     private void fillBlob(int x, int y, int num) {
