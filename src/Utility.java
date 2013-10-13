8602e081f4d9d2e594e98f7240dcd9403d67675a
Chris Wendt <chrismwendt@gmail.com>
2013-10-13 17:23:26 -0400

    Created a utility function which adds an alpha channel to a BufferedImage

--- src/Utility.java
+++ src/Utility.java
@@ -1,2 +1,12 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 public class Utility {
+    public static BufferedImage addAlphaChannel(BufferedImage originalImage) {
+        BufferedImage newImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D newImageGraphics = newImage.createGraphics();
+        newImageGraphics.drawImage(originalImage, 0, 0, null);
+        newImageGraphics.dispose();
+        return newImage;
+    }
 }
