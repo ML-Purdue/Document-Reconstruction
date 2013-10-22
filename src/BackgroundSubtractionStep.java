bf26399800e159f4c69cd4c8e7485f25e966f0d2
Chris Wendt <chrismwendt@gmail.com>
2013-10-22 02:02:41 -0400

    Tracked pressed states of mouse buttons

--- src/BackgroundSubtractionStep.java
+++ src/BackgroundSubtractionStep.java
@@ -4,22 +4,26 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.util.HashSet;
 
 @SuppressWarnings("serial")
-public class BackgroundSubtractionStep extends Step implements MouseMotionListener, KeyListener {
+public class BackgroundSubtractionStep extends Step implements MouseMotionListener, KeyListener, MouseListener {
     BufferedImage originalImage;
     BufferedImage processedImage;
     HashSet<Color> foregroundColors = new HashSet<Color>();
     HashSet<Color> backgroundColors = new HashSet<Color>();
+    boolean mouseLeftDown = false;
+    boolean mouseRightDown = false;
 
     public BackgroundSubtractionStep(Listener listener) {
         super(listener);
 
         setFocusable(true);
         addKeyListener(this);
+        addMouseListener(this);
         addMouseMotionListener(this);
     }
 
@@ -40,11 +44,10 @@ public class BackgroundSubtractionStep extends Step implements MouseMotionListen
 
     @Override
     public void mouseDragged(MouseEvent arg0) {
-        switch (arg0.getButton()) {
-        case MouseEvent.BUTTON1:
+        if (mouseLeftDown == true) {
             foregroundColors.add(new Color(originalImage.getRGB(arg0.getX(), arg0.getY())));
-            break;
-        case MouseEvent.BUTTON2:
+        }
+        if (mouseRightDown == true) {
             backgroundColors.add(new Color(originalImage.getRGB(arg0.getX(), arg0.getY())));
         }
     }
@@ -78,5 +81,41 @@ public class BackgroundSubtractionStep extends Step implements MouseMotionListen
 
     @Override
     public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+        switch (arg0.getButton()) {
+        case MouseEvent.BUTTON1:
+            mouseLeftDown = true;
+            break;
+        case MouseEvent.BUTTON3:
+            mouseRightDown = true;
+            break;
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
+        switch (arg0.getButton()) {
+        case MouseEvent.BUTTON1:
+            mouseLeftDown = false;
+            break;
+        case MouseEvent.BUTTON3:
+            mouseRightDown = false;
+            break;
+        }
     }
 }
