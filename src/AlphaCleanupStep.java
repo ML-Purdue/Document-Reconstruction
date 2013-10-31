b048206318ced7f22d570574c1981b74b8ce5e8b
Chris Wendt <chrismwendt@gmail.com>
2013-10-31 19:43:38 -0400

    Add a skeleton alpha cleanup step

---
+++ src/AlphaCleanupStep.java
@@ -0,0 +1,10 @@
+@SuppressWarnings("serial")
+public class AlphaCleanupStep extends Step {
+    public AlphaCleanupStep(Listener listener) {
+        super(listener);
+    }
+
+    @Override
+    public void begin(Object input) {
+    }
+}
