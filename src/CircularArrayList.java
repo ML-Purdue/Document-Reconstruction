9ccce83def784cdde25db421d0b87c4ac406331b
Chris Wendt <chrismwendt@gmail.com>
2013-11-16 20:01:48 -0500

    Fix constructors

--- src/CircularArrayList.java
+++ src/CircularArrayList.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 public class CircularArrayList<E> extends ArrayList<E> {
-    public CircularArrayList(List<E> other) {
+    public CircularArrayList(Collection<? extends E> other) {
         super(other);
+    }
+
+    public CircularArrayList() {
+        super();
     }
 
     public E get(int i) {
