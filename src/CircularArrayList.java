cee9de7ba116a2653091eeeed50ee825916a497f
Chris Wendt <chrismwendt@gmail.com>
2013-11-16 19:59:06 -0500

    Add a constructor from another list

--- src/CircularArrayList.java
+++ src/CircularArrayList.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class CircularArrayList<E> extends ArrayList<E> {
+    public CircularArrayList(List<E> other) {
+        super(other);
+    }
+
     public E get(int i) {
         return super.get(i % size());
     }
