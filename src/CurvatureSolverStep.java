6785a2b88a59b44baf2667182a38bb4af6fbaf15
Chris Wendt <chrismwendt@gmail.com>
2013-11-18 04:57:06 -0500

    Move matches so that matching sections line up, major WIP
    Updating the island as pieces get attached to it is yet to be done.

--- src/CurvatureSolverStep.java
+++ src/CurvatureSolverStep.java
@@ -20,6 +20,7 @@ public class CurvatureSolverStep extends Step implements Runnable {
     public void begin(Object input) {
         layout = (List<Piece>) input;
         sandbox = makeSandbox(layout);
+        display = sandbox;
         bestError = Double.POSITIVE_INFINITY;
         setPreferredSize(new Dimension(sandbox.getWidth(), sandbox.getHeight()));
 
@@ -29,6 +30,8 @@ public class CurvatureSolverStep extends Step implements Runnable {
     private void solve() {
         // blob detect at 50%
         // construct lists of edges
+        Utility.drawLayout(layout, sandbox);
+        repaint();
         List<List<Double>> curvatures = new ArrayList<List<Double>>();
         List<List<Point>> perimeters = new ArrayList<List<Point>>();
         for (int i = 0; i < layout.size(); i++) {
@@ -36,40 +39,8 @@ public class CurvatureSolverStep extends Step implements Runnable {
             List<Point> perimeter = Utility.perimeter(blob);
             perimeters.add(perimeter);
             List<Double> rawCurvature = Utility.getCurvature(perimeter);
-            List<Double> curvature = Utility.smooth(rawCurvature, 5);
+            List<Double> curvature = Utility.smooth(rawCurvature, 10);
             curvatures.add(curvature);
-
-            Utility.show(layout.get(i).image);
-            BufferedImage image = new BufferedImage(layout.get(i).image.getWidth(), layout.get(i).image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-            double mag = Double.NEGATIVE_INFINITY;
-            for (int j = 0; j < curvature.size(); j++) {
-                mag = Math.max(mag, Math.abs(curvature.get(j)));
-            }
-            for (int j = 0; j < perimeter.size(); j++) {
-                int value = Math.abs((int) (255 / mag * curvature.get(j)));
-                if (curvature.get(j) > 0) {
-                    image.setRGB(perimeter.get(j).x, perimeter.get(j).y, new Color(value, 0, 0).getRGB());
-                } else {
-                    image.setRGB(perimeter.get(j).x, perimeter.get(j).y, new Color(0, value, 0).getRGB());
-                }
-            }
-            Utility.show(image);
-        }
-
-        for (int i = 0; i < layout.size() - 1; i++) {
-            for (int j = i + 1; j < layout.size(); j++) {
-                Utility.CurvatureMatch match = Utility.matchCurvatures(curvatures.get(i), curvatures.get(j));
-                BufferedImage image = new BufferedImage(layout.get(i).image.getWidth(), layout.get(i).image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-                for (int k = 0; k < match.length; k++) {
-                    image.setRGB(perimeters.get(i).get((match.indexA + k) % perimeters.get(i).size()).x, perimeters.get(i).get((match.indexA + k) % perimeters.get(i).size()).y, Color.RED.getRGB());
-                }
-                Utility.show(image);
-                BufferedImage image2 = new BufferedImage(layout.get(j).image.getWidth(), layout.get(j).image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-                for (int k = 0; k < match.length; k++) {
-                    image2.setRGB(perimeters.get(j).get((match.indexB + k) % perimeters.get(j).size()).x, perimeters.get(j).get((match.indexB + k) % perimeters.get(j).size()).y, Color.GREEN.getRGB());
-                }
-                Utility.show(image2);
-            }
         }
 
         // TODO
@@ -78,6 +49,89 @@ public class CurvatureSolverStep extends Step implements Runnable {
         // - find the best match between the island and any other piece using graph comparison of curvatures
         // - combine the island with the best match by moving and rotating it into place
         // - draw the best match on the island
+
+        Piece island = layout.get(0);
+        List<Point> islandPerimeter = perimeters.get(0);
+        List<Double> islandCurvature = curvatures.get(0);
+        layout.remove(0);
+        perimeters.remove(0);
+        curvatures.remove(0);
+
+        while (!layout.isEmpty()) {
+            Utility.CurvatureMatch bestMatch = null;
+            int bestIndex = 0;
+            double bestError = Double.POSITIVE_INFINITY;
+            for (int i = 0; i < layout.size(); i++) {
+                Utility.CurvatureMatch match = Utility.matchCurvatures(islandCurvature, curvatures.get(i));
+                if (match.error < bestError) {
+                    bestError = match.error;
+                    bestMatch = match;
+                    bestIndex = i;
+                }
+            }
+            BufferedImage image = new BufferedImage(layout.get(bestIndex).image.getWidth(), layout.get(bestIndex).image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            for (int k = 0; k < bestMatch.length; k++) {
+                image.setRGB(perimeters.get(bestIndex).get(Utility.mod(bestMatch.indexB + k, perimeters.get(bestIndex).size())).x, perimeters.get(bestIndex).get(Utility.mod(bestMatch.indexB + k, perimeters.get(bestIndex).size())).y, Color.RED.getRGB());
+            }
+            Utility.show(image);
+            image = new BufferedImage(island.image.getWidth(), island.image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            for (int k = 0; k < bestMatch.length; k++) {
+                image.setRGB(islandPerimeter.get((bestMatch.indexA + k) % islandPerimeter.size()).x, islandPerimeter.get((bestMatch.indexA + k) % islandPerimeter.size()).y, Color.RED.getRGB());
+            }
+            Utility.show(image);
+            Vector2D ipA = new Vector2D(islandPerimeter.get(bestMatch.indexA));
+            Vector2D ipB = new Vector2D(islandPerimeter.get((bestMatch.indexA + bestMatch.length) % islandPerimeter.size()));
+            Vector2D is2 = new Vector2D(island.image.getWidth() / 2, island.image.getHeight() / 2);
+            Vector2D ip = new Vector2D(island.position);
+            Vector2D islandA = ip.subtract(is2).add(ipA);
+            Vector2D islandAB = ipB.subtract(ipA);
+
+            Vector2D mpB = new Vector2D(perimeters.get(bestIndex).get(Utility.mod(bestMatch.indexB, perimeters.get(bestIndex).size())));
+            Vector2D mpA = new Vector2D(perimeters.get(bestIndex).get(Utility.mod(bestMatch.indexB + bestMatch.length, perimeters.get(bestIndex).size())));
+            Vector2D ms2 = new Vector2D(layout.get(bestIndex).image.getWidth() / 2, layout.get(bestIndex).image.getHeight() / 2);
+            Vector2D mp = new Vector2D(layout.get(bestIndex).position);
+            Vector2D matchA = mp.subtract(ms2).add(mpA);
+            Vector2D matchAB = mpB.subtract(mpA);
+
+            double angle = matchAB.angleBetween(islandAB);
+            Vector2D bestPosition = new Vector2D(layout.get(bestIndex).position);
+            Vector2D delta = matchA.subtract(bestPosition).rotate(angle).add(bestPosition).subtract(islandA);
+            layout.get(bestIndex).position = bestPosition.subtract(delta);
+            layout.get(bestIndex).rotation += angle;
+            BufferedImage temp = new BufferedImage(sandbox.getWidth(), sandbox.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            Utility.drawPiece(island, temp);
+            Utility.drawPiece(layout.get(bestIndex), temp);
+            layout.remove(bestIndex);
+            perimeters.remove(bestIndex);
+            curvatures.remove(bestIndex);
+
+            islandPerimeter = Utility.perimeter(Utility.getLargestBlob(island.image, 128));
+            islandCurvature = Utility.smooth(Utility.getCurvature(islandPerimeter), 5);
+
+            Utility.drawChecker(sandbox.getGraphics(), sandbox.getWidth(), sandbox.getHeight(), 10, Color.LIGHT_GRAY, Color.DARK_GRAY);
+            Utility.show(island.image);
+            Utility.drawPiece(island, sandbox);
+            Vector2D matchedA = matchA.rotate(angle, bestPosition).subtract(delta);
+            Vector2D matchedAB = matchAB.rotate(angle);
+            Graphics g = temp.getGraphics();
+            g.setColor(Color.WHITE);
+            g.drawLine((int) islandA.x, (int) islandA.y, (int) matchA.x, (int) matchA.y);
+            g.setColor(Color.GREEN);
+            g.drawLine((int) islandA.x, (int) islandA.y, (int) islandA.x + (int) islandAB.x, (int) islandA.y + (int) islandAB.y);
+            g.setColor(Color.RED);
+            g.drawLine((int) matchA.x, (int) matchA.y, (int) matchA.x + (int) matchAB.x, (int) matchA.y + (int) matchAB.y);
+            g.setColor(Color.RED);
+            g.drawLine((int) matchedA.x, (int) matchedA.y, (int) matchedA.x + (int) matchedAB.x, (int) matchedA.y + (int) matchedAB.y);
+            g.drawRect((int) (island.position.x - island.image.getWidth() / 2), (int) (island.position.y - island.image.getHeight() / 2), island.image.getWidth(), island.image.getHeight());
+            Utility.show(temp);
+            repaint();
+            System.out.println();
+
+            // set bestPiece.rotation and position
+            // draw bestPiece on match
+            // recalculate island blob, perimeter, curvature
+            // draw layout on checker background in sandbox
+        }
     }
 
     @Override
