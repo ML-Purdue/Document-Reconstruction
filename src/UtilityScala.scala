928d3c63a15f5829150167076d7b9b6980306803
arya0 <arya0@purdue.edu>
2014-03-01 16:02:56 -0500

    Stufffff

--- src/UtilityScala.scala
+++ src/UtilityScala.scala
@@ -1,22 +1,35 @@
 import java.awt.image.BufferedImage;
 import java.awt.Color;
-
+import java.awt.geom.Point2D;
+import java.util.List;
 object UtilityScala {
-  def showAlpha(image: BufferedImage): BufferedImage = {
-    val newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-    for (iy <- 0 until image.getHeight(); ix <- 0 until image.getWidth()) {
-      System.out.println("(" + ix + ", " + iy + ")");
-      val alpha = new Color(image.getRGB(ix, iy), true).getAlpha();
-      newImage.setRGB(ix, iy, new Color(alpha, alpha, alpha).getRGB());
+  def showAlpha(image: BufferedImage): BufferedImage =
+    {
+      val newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+      for (iy <- 0 until image.getHeight(); ix <- 0 until image.getWidth()) {
+        System.out.println("(" + ix + ", " + iy + ")");
+        val alpha = new Color(image.getRGB(ix, iy), true).getAlpha();
+        newImage.setRGB(ix, iy, new Color(alpha, alpha, alpha).getRGB());
+      }
+      return newImage;
     }
-    return newImage;
-  }
 
-  def lerp(a: Double, b: Double, t: Double): Double = {
-    return a + (b - a) * t;
-  }
+  def lerp(a: Double, b: Double, t: Double): Double =
+    {
+      return a + (b - a) * t;
+    }
 
-  def unlerp(a: Double, b: Double, v: Double): Double = {
-    return (v - a) / (b - a);
-  }
+  def unlerp(a: Double, b: Double, v: Double): Double =
+    {
+      return (v - a) / (b - a);
+    }
+
+  /*def listPoint2DToVector2D(l: List<Point2D.Double>): List<Vector2D> =
+  {
+    val lp = new ArrayList<Vector2D>();
+    for (Point2D.Double p : l) {
+        lp.add(new Vector2D(p));
+    }
+    return lp;
+  }*/
 }
